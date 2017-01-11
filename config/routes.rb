17d12962fb94310ebea06bec43bb00a6133d390d
kbanico <code4uguam@gmail.com>
2017-01-11 10:30:38 +0000

    add section page not working

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show]
 
    namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
+    resources :courses, only: [:new, :create, :show] do
+    resources :sections, only: [:new, :create]
   end
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
