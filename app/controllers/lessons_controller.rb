1962348100b1c37428cb0ff408a309e07e0a188b
kbanico <code4uguam@gmail.com>
2017-01-13 12:12:43 +0000

    challenge not working

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,22 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
+    # if current_user.enrolled_in(current_lesson.section.course.id)
+    #   redirect_to courses_path(current_lesson.course)
+    # end
 
   end
 
   private
+    def require_authorized_for_current_lesson
+      if current_lesson.section.course.id != current_user.enrollments.course.id
+        render text: "Unauthorized", status: :unauthorized
+      end
+    end
+
+
     helper_method :current_lesson
     def current_lesson
       @current_lesson ||= Lesson.find(params[:id])
